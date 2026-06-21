ad2c979b5f4809e8a8fa966f1bf56f776dea5a90
DikshantBadawadagi <dikshantbadawadagi@gmail.com>
2026-06-22 00:15:15 +0530

    CRUDforPatient

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, HTTPException
 import json
 
 app = FastAPI()
@@ -17,3 +17,12 @@ def hello():
 def getPatients():
     data = load_data()
     return data
+
+@app.get('/patient/{patient_id}')
+def getPatient(patient_id : str = Path(..., description="ID of the patient", examples="P001")):
+    data = load_data()
+
+    if patient_id in data:
+        return data[patient_id]
+    else:
+        raise HTTPException(status_code=404, detail="Patient not found")
